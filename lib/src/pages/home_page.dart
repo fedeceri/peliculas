8fc6a759b71d4e06dd956e8ddb6b346e54669a91
Federico <cerianifede@gmail.com>
2020-01-15 13:36:37 -0300

    "otro"

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -10,6 +10,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    peliculasProvider.getPopulares();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Películas de Cine'),
@@ -68,11 +71,14 @@ class HomePage extends StatelessWidget {
             child: Text('Populares', style: Theme.of(context).textTheme.subhead)
             ),
           SizedBox(height: 5.0),
-          FutureBuilder(
-            future: peliculasProvider.getPopulares(),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if(snapshot.hasData){
-                return MovieHorizontal(peliculas: snapshot.data);
+                return MovieHorizontal(
+                  peliculas: snapshot.data,
+                  siguientePagina: peliculasProvider.getPopulares, 
+                  );
               }else{
                 return Center(child: CircularProgressIndicator());
               }
